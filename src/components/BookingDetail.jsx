97d69af74bf3c53cd893c0f738c62b023ef8524b
규.이규민 <gmlee@lunasoft.co.kr>
2023-03-26 00:01:52 +0900

    예약정보 스타일 작업

--- src/components/BookingDetail.jsx
+++ src/components/BookingDetail.jsx
@@ -4,7 +4,45 @@ import Component from '@core/Component';
 export default class BookingDetail extends Component {
     template() {
         return (
-            <div>예약 정보 및 고객 정보</div>
+            <div class='booking-detail'>
+                <div class='detail-box'>
+                    <div class='title'>예약 정보</div>
+                    <div class="detail">
+                        <div class="detail-item">
+                            <div class='subtitle'>예약 상태</div>
+                            <div class="content">value</div>
+                        </div>
+                        <div class="detail-item">
+                            <div class='subtitle'>예약 시간</div>
+                            <div class="content">value</div>
+                        </div>
+                        <div class="detail-item">
+                            <div class='subtitle'>접수 시간</div>
+                            <div class="content">value</div>
+                        </div>
+                    </div>
+                    <div class='title'>고객 정보</div>
+                    <div class="detail">
+                        <div class="detail-item">
+                            <div class='subtitle'>고객 성명</div>
+                            <div class="content">value</div>
+                        </div>
+                        <div class="detail-item">
+                            <div class='subtitle'>고객 등급</div>
+                            <div class="content">value</div>
+                        </div>
+                        <div class="detail-item">
+                            <div class='subtitle'>고객 메모</div>
+                            <div class="content">value</div>
+                        </div>
+                        <div class="detail-item">
+                            <div class='subtitle'>요청 사항</div>
+                            <div class="content">value</div>
+                        </div>
+                    </div>
+                </div>
+                <div class='detail-dim'></div>
+            </div>
         )
     }
 }
